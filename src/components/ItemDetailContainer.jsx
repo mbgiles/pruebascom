8cee456376ecb90a47e1786fae9dc39ba7539447
magali giles <mgiles@com.laplata.gov.ar>
2024-06-18 12:04:14 -0300

    css

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,25 +1,3 @@
-// import React, { useEffect, useState } from 'react'
-// import { useParams } from 'react-router-dom'
-// import data from "../data/productos.json";
-
-// const ItemDetailContainer = () => {
-
-//     let { itemId } = useParams();
-//     let [producto, setProducto] = useState(undefined);
-
-//     useEffect(() => {
-//         setProducto(data.find((prod) => prod.id === parseInt(itemId)));
-//     }, [itemId])
-    
-
-//   return (
-//     <div>{producto ? producto.nombre : "Cargando..."}</div>
-//   )
-// }
-
-// export default ItemDetailContainer
-
-// ItemDetailContainer.jsx
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import data from "../data/productos.json";
